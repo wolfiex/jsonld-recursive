e1e42f0271b38b3c243c0650a2cdaff658707352
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-04-13 12:00:52 +0100

    fix: switch to setuptools_scm dynamic versioning from git tags

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,5 +5,8 @@ jsonld-recursive Python client library
 from .ldr_client import LdrClient
 from .test_install import test, get_test_data_dir
 
-__version__ = "1.0.0"
+try:
+    from .version import __version__
+except ImportError:
+    __version__ = "unknown"
 __all__ = ["LdrClient", "test", "get_test_data_dir"]
